3ca8055f88223bdcd9c3df7a5bfd2895fc70cffd
oleg <olegsssq@gmail.com>
2026-07-24 12:48:35 +0300

    Update js and css

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,3 +1,10 @@
+function toggleTheme() {
+  const current = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light';
+  const next = current === 'dark' ? 'light' : 'dark';
+  document.documentElement.setAttribute('data-theme', next);
+  localStorage.setItem('theme', next);
+}
+
 function toggleSwitch(el) {
   el.classList.toggle('on');
 }
